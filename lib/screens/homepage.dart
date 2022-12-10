02380b0d1970abd59c2624a52ddbe881776f8467
Fcristiii3 <cristian_007_gabriel@yahoo.com>
2022-12-10 14:25:11 +0200

    added search bar

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -8,11 +8,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('PhamaChain')),
-      body: Column(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _myController,
+              decoration: InputDecoration(border: OutlineInputBorder(),suffixIcon: IconButton(icon: Icon(Icons.search),onPressed: (){},),label: Text("Enter a product")),
+
+            ),
+          )
+        ],
+      ),
     );
   }
 }
